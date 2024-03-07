e3d8ca058a40f50fc41988b860eef067357e8e96
Fernandasoares <nandasoaress2157@gmail.com>
2024-03-07 16:51:36 -0300

    Atualização do filtro e LocalStorage

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,12 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid';
 import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
 uuidv4();
 
+const loadTodosFromLocalStorage = () => {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
+
+const saveTodosToLocalStorage = (todos) => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
 export const TodoWrapper = ({ handleFilterChange, filter }) => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(loadTodosFromLocalStorage());
+
+  useEffect(() => {
+    saveTodosToLocalStorage(todos); // Salvar os todos no localStorage sempre que eles mudarem
+  }, [todos]);
 
   const addTodo = todo => {
       setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
@@ -15,7 +28,7 @@ export const TodoWrapper = ({ handleFilterChange, filter }) => {
 
   const toggleComplete = id => {
     setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
-  }
+}
 
   const deleteTodo = id => {
     setTodos(todos.filter(todo => todo.id !== id))
